5181882f2e096eac1e501fa07964830706d16998
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-11-11 17:04:13 -0800

    added : Functionality of creating addressBook contacts is added

--- AddressBook.js
+++ AddressBook.js
@@ -1,2 +1,28 @@
 //AddressBook Programming
 console.log("Welcome to addressBook programming");
+
+//UC1
+class Contact {
+
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+    //constructors
+    constructor(...parameter) {
+        this.firstName = parameter[0];
+        this.lastName = parameter[1];
+        this.address = parameter[2];
+        this.city = parameter[3];
+        this.state = parameter[4];
+        this.zip = parameter[5];
+        this.phoneNumber = parameter[6];
+        this.email = parameter[7];
+    }
+}
+let contact1 = new Contact("Ashwini", "Jadi", "dskr", "Hyderabad", "Telangana", 500025, 7899963310, "ashu@gmail.com");
+console.log(contact1); 
